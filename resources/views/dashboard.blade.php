7175321126baaf351d53a658f411b51f22a874a5
Daniil Kunitcyn <danchik.kun@mail.ru>
2022-10-17 16:08:33 +0400

    Курсор ахуенный но надо переделать

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset("css/dashboard.css") }}">
+    <link rel="stylesheet" href="{{ asset("css/adaptive.css") }}">
+    <link rel="stylesheet" href="{{ asset("css/fonts.css") }}">
+    <title>{{ Auth::user()->name }}</title>
+</head>
+<body>
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
     </x-slot>
+</x-app-layout>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+<section>
+    <div class="info_about">
+        <div class="avatar_logout">
+            <div class="ava"></div>
+            <p>{{ Auth::user()->name }}</p>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <x-dropdown-link class="logout" :href="route('logout')"
+                        onclick="event.preventDefault();
+                                    this.closest('form').submit();">
+                    {{ __('Выйти') }}
+                </x-dropdown-link>
+            </form>
+        </div>
+        <div class="info_user">
+            <div class="log_email">
+                <p>Логин: {{ Auth::user()->name }}</p>
+                <p>E-mail: {{ Auth::user()->email }}</p>
             </div>
         </div>
     </div>
-    <a href="/">На Главную</a>
-</x-app-layout>
+</section>
+
+<section class="form_outback">
+<h3><span>—</span> Øбратная связь <span>—</span></h3>
+    <div class="form_out">
+        <form action="">
+            <textarea name="" placeholder="Введите свое сообщение :" id="" cols="41" rows="10"></textarea>
+            <button type="submit">Подписаться</button>
+        </form>
+    </div>
+</section>
+
+<footer>
+        <img class="mobile_footer" src="{{ asset("img/logofooter.png") }}" alt="">
+        <ul class="navigation_footer">
+            <li><img src="{{ asset("img/logofooter.png") }}" alt=""></li>
+            <li>Главная</li>
+            <li>О нас</li>
+            <li>Каталог</li>            
+            <li>Магазин</li>
+            <li>Контакты</li>
+        </ul>
+        <a href="">Политика конфидециальности</a>
+        <div class="cop">
+            <p>LeafTea  ©  2022 Все права защищены</p>
+        </div>
+    </footer>
+</body>
+</html>
